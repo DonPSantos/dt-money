4d25fb24d891e43ddb7687f0c275b52004aceea2
DonPSantos <donrock333@gmail.com>
2023-02-01 21:28:31 -0300

    Validações de formularios

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -9,6 +9,7 @@ export const SearchFormContainer = styled.form`
         border-radius:6px;
         border:0;
         background:${props => props.theme["gray-900"]};
+        color: ${props => props.theme["gray-300"]};
         padding:1rem;
 
         &::placeholder{
@@ -29,7 +30,12 @@ export const SearchFormContainer = styled.form`
         font-weight:bold;
         border-radius:6px;
 
-        &:hover{
+        &:disabled{
+            opacity:0.7;
+            cursor:not-allowed;
+        }
+
+        &:not(:disabled):hover{
             background:${props => props.theme["green-500"]};
             border: ${props => props.theme["green-500"]};
             color:${props => props.theme.white};
